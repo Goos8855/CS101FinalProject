80b2b1fce1dcefd02dffc433751dcc4036143fa4
Ray <119020664+Goos8855@users.noreply.github.com>
2025-12-18 17:19:53 +0000

    Define struct and open file

--- main.cpp
+++ main.cpp
@@ -1,5 +1,27 @@
 #include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+
 using namespace std;
+
+struct point{ //Organize our points into a vector list of points
+    int x;
+    int y;
+    char txt;
+};
+
 int main(){
+    ifstream file("data.txt");
+    if(!file){ //Check if the file actually opens first
+        cout << "File couldn't open" << endl;
+        return 1;
+    }
+
+    vector<point> points;
+    int x;
+    int y;
+    char txt;
+
     
 }
